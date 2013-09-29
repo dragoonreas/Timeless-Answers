4dacd6900d98d642ca9129e109b3a51ac8b1e4da
Colin Friend <dragoonreas@gmail>
2013-09-30 01:22:38 +1000

    Added localisation support
    Added localisation support via AceLocale-3.0 and the online Curseforge
    Localisation Application.

--- TimelessAnswers.lua
+++ TimelessAnswers.lua
@@ -16,66 +16,76 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]--
 
+-- Get addon name
+local addon = ...
+
 -- Create frame
-local frame = CreateFrame("FRAME", "TimelessAnswers");
+local frame = CreateFrame("FRAME", addon);
 
 -- Register frame with quest dialogue events
 frame:RegisterEvent("GOSSIP_SHOW");
 frame:RegisterEvent("QUEST_DETAIL");
 frame:RegisterEvent("QUEST_COMPLETE");
 
+-- Get locale strings table
+local silent = true
+--@debug@
+silent = false
+--@end-debug@
+local L = LibStub("AceLocale-3.0"):GetLocale(addon, silent)
+
 -- Create question/answer table
 local questions = {
-    ["Draka's Fury."] = "This Horde ship was crafted by goblins. Originally intended to bring Thrall and Aggra to the Maelstrom, the ship was destroyed in a surprise attack by the Alliance.",
-    ["Mur'ghouls."] = "Tell me, hero, what are undead murlocs called?",
-    ["Mirador."] = "What is the name of Tirion Fordring's gray stallion?",
-    ["Tiffin Ellerian Wrynn."] = "Which of these is the correct name for King Varian Wrynn's first wife?",
-    ["Xavius."] = "Who was the first satyr to be created?",
-    ["Calissa Harrington."] = "Before Ripsnarl became a worgen, he had a family. What was his wife's name?",
-    ["Coilfang Reservoir."] = "This structure, located in Zangarmarsh, was controlled by naga who sought to drain a precious and limited resource: the water of Outland.",
-    ["Mueh'zala."] = "One name for this loa is \"Night's Friend\".",
-    ["Holia Sunshield."] = "This defender of the Scarlet Crusade was killed while slaying the dreadlord Beltheris.",
-    ["Mag'har."] = "Brown-skinned orcs first began showing up on Azeroth several years after the Third War, when the Dark Portal was reactivated. What are these orcs called?",
-    ["Sayaad."] = "Succubus demons revel in causing anguish, and they serve the Legion by conducting nightmarish interrogations. What species is the succubus?",
-    ["Giles."] = "While working as a tutor, Stalvan Mistmantle became obsessed with one of his students, a young woman named Tilloa. What was the name of her younger brother?",
-    ["Veranus."] = "Who was the mighty proto-dragon captured by Loken and transformed into Razorscale?",
-    ["Tatai."] = "This emissary of the Horde felt that Silvermoon City was a little too bright and clean.",
-    ["Queen Mia Greymane."] = "This queen oversaw the evacuation of her people after the Cataclysm struck and the Forsaken attacked her nation.",
-    ["Talak."] = "Not long ago, this frail Zandalari troll sought to tame a direhorn. Although he journeyed to the Isle of Giants, he was slain in his quest. What was his name?",
-    ["Acherus."] = "Arthas's death knights were trained in a floating citadel that was taken by force when many of them rebelled against the Lich King. What was the fortress's name?",
-    ["Frostwolf clan."] = "White wolves were once the favored mounts of which orc clan?",
-    ["Sharp claw."] = "In Taur-ahe, the language of the tauren, what does lar'korwi mean?",
-    ["K'aresh."] = "Name the homeworld of the ethereals.",
-    ["Teron Gorefiend."] = "Who was the first death knight to be created on Azeroth?",
-    ["Mord'rethar."] = "In the assault on Icecrown, Horde forces dishonorably attacked Alliance soldiers who were busy fighting the Scourge and trying to capture this gate.",
-    ["Tainted grain."] = "What evidence drove Prince Arthas to slaughter the people of Stratholme during the Third War?",
-    ["Gelbin Mekkatorque."] = "Who is the current leader of the gnomish people?",
-    ["Cenarion Circle."] = "Malfurion Stormrage helped found this group, which is the primary druidic organization of Azeroth.",
-    ["Defective elekk turd."] = "The draenei like to joke that in the language of the naaru, the word Exodar has this meaning.",
-    ["Sky'ree."] = "Thane Kurdran Wildhammer recently suffered a tragic loss when his valiant gryphon was killed in a fire. What was this gryphon's name?",
-    ["Blue dragonflight."] = "Before she was raised from the dead by Arthas to serve the Scourge, Sindragosa was a part of what dragonflight?",
-    ["Toothgnasher."] = "The Ironforge library features a replica of an unusually large ram's skeleton. What was the name of this legendary ram?",
-    ["Norgannon."] = "Name the titan lore-keeper who was a member of the elite Pantheon.",
-    ["Nordrassil."] = "What did the Dragon Aspects give the night elves after the War of the Ancients?",
-    ["Nobundo."] = "Formerly a healthy paladin, this draenei fell ill after fighting the Burning Legion and becoming one of the Broken. He later became a powerful shaman.",
-    ["Tenebron, Vesperon, and Shadron."] = "Who were the three young twilight drakes guarding twilight dragon eggs in the Obsidian Sanctum?",
-    ["Belan shi."] = "What phrase means \"Thank you\" in Draconic, the language of dragons?",
-    ["Red pox."] = "Before the original Horde formed, a highly contagious sickness began spreading rapidly among the orcs. What did the orcs call it?",
-    ["Archdruid."] = "What is the highest rank bestowed on a druid?",
-    ["King Terenas Menethil II."] = "Whose tomb includes the inscription \"May the bloodied crown stay lost and forgotten\"?",
+    [ L["Draka's Fury."] ] = L["This Horde ship was crafted by goblins. Originally intended to bring Thrall and Aggra to the Maelstrom, the ship was destroyed in a surprise attack by the Alliance."],
+    [ L["Mur'ghouls."] ] = L["Tell me, hero, what are undead murlocs called?"],
+    [ L["Mirador."] ] = L["What is the name of Tirion Fordring's gray stallion?"],
+    [ L["Tiffin Ellerian Wrynn."] ] = L["Which of these is the correct name for King Varian Wrynn's first wife?"],
+    [ L["Xavius."] ] = L["Who was the first satyr to be created?"],
+    [ L["Calissa Harrington."] ] = L["Before Ripsnarl became a worgen, he had a family. What was his wife's name?"],
+    [ L["Coilfang Reservoir."] ] = L["This structure, located in Zangarmarsh, was controlled by naga who sought to drain a precious and limited resource: the water of Outland."],
+    [ L["Mueh'zala."] ] = L["One name for this loa is \"Night's Friend\"."],
+    [ L["Holia Sunshield."] ] = L["This defender of the Scarlet Crusade was killed while slaying the dreadlord Beltheris."],
+    [ L["Mag'har."] ] = L["Brown-skinned orcs first began showing up on Azeroth several years after the Third War, when the Dark Portal was reactivated. What are these orcs called?"],
+    [ L["Sayaad."] ] = L["Succubus demons revel in causing anguish, and they serve the Legion by conducting nightmarish interrogations. What species is the succubus?"],
+    [ L["Giles."] ] = L["While working as a tutor, Stalvan Mistmantle became obsessed with one of his students, a young woman named Tilloa. What was the name of her younger brother?"],
+    [ L["Veranus."] ] = L["Who was the mighty proto-dragon captured by Loken and transformed into Razorscale?"],
+    [ L["Tatai."] ] = L["This emissary of the Horde felt that Silvermoon City was a little too bright and clean."],
+    [ L["Queen Mia Greymane."] ] = L["This queen oversaw the evacuation of her people after the Cataclysm struck and the Forsaken attacked her nation."],
+    [ L["Talak."] ] = L["Not long ago, this frail Zandalari troll sought to tame a direhorn. Although he journeyed to the Isle of Giants, he was slain in his quest. What was his name?"],
+    [ L["Acherus."] ] = L["Arthas's death knights were trained in a floating citadel that was taken by force when many of them rebelled against the Lich King. What was the fortress's name?"],
+    [ L["Frostwolf clan."] ] = L["White wolves were once the favored mounts of which orc clan?"],
+    [ L["Sharp claw."] ] = L["In Taur-ahe, the language of the tauren, what does lar'korwi mean?"],
+    [ L["K'aresh."] ] = L["Name the homeworld of the ethereals."],
+    [ L["Teron Gorefiend."] ] = L["Who was the first death knight to be created on Azeroth?"],
+    [ L["Mord'rethar."] ] = L["In the assault on Icecrown, Horde forces dishonorably attacked Alliance soldiers who were busy fighting the Scourge and trying to capture this gate."],
+    [ L["Tainted grain."] ] = L["What evidence drove Prince Arthas to slaughter the people of Stratholme during the Third War?"],
+    [ L["Gelbin Mekkatorque."] ] = L["Who is the current leader of the gnomish people?"],
+    [ L["Cenarion Circle."] ] = L["Malfurion Stormrage helped found this group, which is the primary druidic organization of Azeroth."],
+    [ L["Defective elekk turd."] ] = L["The draenei like to joke that in the language of the naaru, the word Exodar has this meaning."],
+    [ L["Sky'ree."] ] = L["Thane Kurdran Wildhammer recently suffered a tragic loss when his valiant gryphon was killed in a fire. What was this gryphon's name?"],
+    [ L["Blue dragonflight."] ] = L["Before she was raised from the dead by Arthas to serve the Scourge, Sindragosa was a part of what dragonflight?"],
+    [ L["Toothgnasher."] ] = L["The Ironforge library features a replica of an unusually large ram's skeleton. What was the name of this legendary ram?"],
+    [ L["Norgannon."] ] = L["Name the titan lore-keeper who was a member of the elite Pantheon."],
+    [ L["Nordrassil."] ] = L["What did the Dragon Aspects give the night elves after the War of the Ancients?"],
+    [ L["Nobundo."] ] = L["Formerly a healthy paladin, this draenei fell ill after fighting the Burning Legion and becoming one of the Broken. He later became a powerful shaman."],
+    [ L["Tenebron, Vesperon, and Shadron."] ] = L["Who were the three young twilight drakes guarding twilight dragon eggs in the Obsidian Sanctum?"],
+    [ L["Belan shi."] ] = L["What phrase means \"Thank you\" in Draconic, the language of dragons?"],
+    [ L["Red pox."] ] = L["Before the original Horde formed, a highly contagious sickness began spreading rapidly among the orcs. What did the orcs call it?"],
+    [ L["Archdruid."] ] = L["What is the highest rank bestowed on a druid?"],
+    [ L["King Terenas Menethil II."] ] = L["Whose tomb includes the inscription \"May the bloodied crown stay lost and forgotten\"?"],
 };
 
 -- Custom print function
 local function Print(msg, isError)
 	-- Append coloured prefix to output
-	if isError then print(format("|cFFFFFF00TA -|r |cFFFF0000Error:|r %s", msg));
-	else print(format("|cFFFFFF00TA -|r %s", msg)); end
+	if isError then print( format("|cFFFFFF00%s -|r |cFFFF0000%s:|r %s", L["TA"], L["Error"], msg) );
+	else print( format("|cFFFFFF00%s -|r %s", L["TA"], msg) ); end
 end
 
 -- Make function to call when regeistered events for quest dialogues are called
 local function frame_OnEvent(self, event, ...)
     -- Check if Senior Historian Evelyna is targeted
-    if UnitExists("target") and tonumber(UnitGUID("target"):sub(6, 10), 16) == 73570 then
+    if UnitExists("target") and tonumber( UnitGUID("target"):sub(6, 10), 16 ) == 73570 then
         --Print(format("%s event fired.", event));
         -- Check event type
         if event == "QUEST_DETAIL" then
@@ -94,9 +104,9 @@ local function frame_OnEvent(self, event, ...)
                 -- Try and find the question so we can get the answer
                 local qFound, aFound = false, false;
                 for a, q in pairs(questions) do
-                    if question == "Let us test your knowledge of history, then! " .. q then
+                    if question == L["Let us test your knowledge of history, then! "] .. q then
                         qFound = true;
-                        Print(format("|cFF00FF00Found Question:|r %s", q));
+                        Print( format("|cFF00FF00%s:|r %s", L["Found Question"], q) );
                         
                         -- Now that we know the answer, try and find it in the given options
                         for i, o in pairs(options["text"]) do
@@ -104,24 +114,24 @@ local function frame_OnEvent(self, event, ...)
                                 aFound = true;
                                 -- Select the option with the correct answer
                                 SelectGossipOption(i);
-                                Print(format("|cFF00FF00Found Answer:|r Option %d. %s", i, o));
+                                Print( format("|cFF00FF00%s:|r %s %d. %s", L["Found Answer"], L["Option"], i, o) );
                                 
                                 break;
                             end
                         end
                         
                         -- Blizzard's changed the quest text since the PTR...
-                        if aFound == false then Print(format("Answer of \"%s\" for question \"%s\" not found in gossip options.", a, q), true); end
+                        if aFound == false then Print( format( L["Answer of \"%s\" for question \"%s\" not found in gossip options."], a, q ), true ); end
                         
                         break;
                     end
                 end
                 
                 -- Blizzard's added questions since the PTR...
-                if qFound == false then Print(format("Question from \"%s\" not found.", question), true); end
+                if qFound == false then Print( format( L["Question from \"%s\" not found."], question ), true ); end
                 
             -- Check if we've already answered the question
-            elseif GetGossipText() == "That is correct!" then
+            elseif GetGossipText() == L["That is correct!"] then
 				-- Close the conglatulatory dialogue if the question's been answered (we cheated so we don't deserve it anyway)
 				CloseGossip();
 				--Print("Congratulatory message closed.");
@@ -140,4 +150,4 @@ end
 frame:SetScript("OnEvent", frame_OnEvent);
 
 -- Output that the addon has loaded
-Print("Timeless Answers Loaded.");
+Print( L["Timeless Answers Loaded."] );
